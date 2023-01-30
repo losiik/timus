0d54db720d1404e20e71211f36a2ad8f6311c7f3
losiik <49565548+losiik@users.noreply.github.com>
2023-01-30 15:59:57 +0400

    timus tasks

--- 1910.py
+++ 1910.py
@@ -4,24 +4,15 @@ magic_fields = input()
 magic_fields = magic_fields.split()
 magic_fields = list(map(int, magic_fields))
 
-# sum_fields: position
-output_values = {}
+result = 0
+pos = 0
+final_result = 0
 
-field_temp = magic_fields[0]
-counter = 1
-sum_fields = field_temp
+for i in range(n - 2):
+    result += magic_fields[i] + magic_fields[i + 1] + magic_fields[i + 2]
+    if result > final_result:
+        final_result = result
+        pos = i + 2
+    result = 0
 
-for i in range(1, len(magic_fields)):
-    if magic_fields[i] == field_temp:
-        counter += 1
-        sum_fields += field_temp
-    else:
-        counter = 1
-        field_temp = magic_fields[i]
-        sum_fields = field_temp
-
-    if counter == 3:
-        output_values[sum_fields] = i
-
-max_key = max(list(output_values))
-print(max_key, output_values[max_key])
+print(final_result, pos)
